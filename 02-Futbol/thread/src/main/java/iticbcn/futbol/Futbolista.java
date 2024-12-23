964fb745ade23999b77be7506d4083f86827d0f8
Lishi Ji Liu <liulishij@gmail.com>
2024-12-23 20:35:57 +0100

    Activitat 02-Futbol: commit inicial, no vaig assistir a classe

---
+++ 02-Futbol/thread/src/main/java/iticbcn/futbol/Futbolista.java
@@ -0,0 +1,10 @@
+package iticbcn.futbol;
+/* Classe que esten Thread, que inicialitza el nombre de gols
+ * i el nombre de tirades a 0 dins del constructor.
+ * 
+ * A part té 3 constants NUM_JUGADORS 11, NUM_TIRADES 20 i la
+ * PROBABILITAT de 0.5f.
+ */
+public class Futbolista {
+    
+}
---
+++ 02-Futbol/thread/src/main/java/iticbcn/futbol/MainDemoFil.java
@@ -0,0 +1,7 @@
+package iticbcn.futbol;
+/* Classe que a partir de la captura del fil actual en execució,
+ * mostra la seva prioritat, nom i el toString()
+ */
+public class MainDemoFil {
+    
+}
