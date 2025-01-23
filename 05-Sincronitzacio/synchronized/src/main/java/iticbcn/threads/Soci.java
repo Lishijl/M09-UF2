12ea30853b8a31c85170c8a2fc5bf37ca3b6eafc
Lishi Ji Liu <liulishij@gmail.com>
2025-01-23 19:02:16 +0100

    05-sincronització fet a classe

--- 05-Sincronitzacio/synchronized/src/main/java/iticbcn/threads/Soci.java
+++ 05-Sincronitzacio/synchronized/src/main/java/iticbcn/threads/Soci.java
@@ -1,5 +1,38 @@
 package iticbcn.threads;
 
-public class Soci {
-    
+import java.util.Random;
+
+public class Soci extends Thread{
+    private final Compte COMPTE;
+    private final float APORTACIO;
+    private final int ESPERA_MAX;
+    private final Random RND;
+    private final int MAX_ANYS;
+
+    public Soci() {
+        this.COMPTE = Compte.getInstance();
+        this.APORTACIO = 10f;
+        this.ESPERA_MAX = 100;
+        this.RND = new Random();
+        this.MAX_ANYS = 10;
+    }
+
+    public Compte getCompte() { return this.COMPTE; }
+
+    // el procediment que fara tots els socis al llarg de 10 anys
+    @Override
+    public void run() {
+        float saldoCompte = this.COMPTE.getSaldo();
+        for (int any = 0; any < this.MAX_ANYS; any++) {
+            for (int mes = 0; mes < 12; mes++) {
+                // ingresa quota
+                if (mes%2 == 0) {
+                    this.COMPTE.setSaldo(this.APORTACIO);
+                } else {
+                    // treu quota amb una aportació negativa
+                    this.COMPTE.setSaldo(-this.APORTACIO);
+                }
+            }
+        }
+    }
 }
