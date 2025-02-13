c866d8fc18a0500750f789c58af5fd76cf5ed442
Lishi Ji Liu <liulishij@gmail.com>
2025-02-13 05:49:49 +0100

    07-Filòsofs finalitzat, va de jugar amb els sleeps per aconseguir l'equilibri

--- 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Filosof.java
+++ 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Filosof.java
@@ -1,9 +1,88 @@
 package iticbcn.threads;
 
+import java.util.Random;
+
 public class Filosof extends Thread {
-    // completar propietats, comptador, nom per filosof, metodes alternatius i run
-    public Filosof() {}
+    private Forquilla forkR;
+    private Forquilla forkL;
+
+    private int gana;
+    private Random rnd;
+
+    public Filosof(int numFil) {
+        super(Integer.toString(numFil));
+        this.gana = 0;
+        this.rnd = new Random();
+    }
+
+    public void menjar() {
+        System.out.printf("Filòsof: fil%s menja\n", getName());
+        dura(1000, 2001);
+        System.out.printf("Filòsof: fil%s ha acabat de menjar\n", getName());
+        this.gana = 0;
+    }
+    public void pensar() {
+        System.out.printf("Filòsof: fil%s pensant\n", getName());
+        dura(1000, 2001);
+    }
+
+    public void dura(int org, int limit) {
+        try {
+            sleep(rnd.nextInt(org, limit));
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void passarGana() {
+        gana++;
+        System.out.printf("Filòsof: fil%s gana=%d\n", getName(), gana);
+        dura(500, 1001);
+    }
 
     @Override
-    public void run() {}
+    public void run() {
+        // menja i pensa alternativament
+        while (true) {
+            if (!forkL.isEnUs()) {
+                forkL.setEnUs(true);
+                System.out.printf("Filòsof: fil%s agafa la forquilla esquerra %d\n", getName(), forkL.getNumF());
+                // cas 1 : pot menjar
+                if (!forkR.isEnUs()) {
+                    forkR.setEnUs(true);
+                    System.out.printf("Filòsof: fil%s agafa la forquilla dreta %d\n", getName(), forkR.getNumF());
+                    menjar();
+                    // torna a deixar forquilles
+                    forkL.setEnUs(false);
+                    forkR.setEnUs(false);
+                } else {
+                    // cas 2: no pot menjar, no serveix 1 sola forquilla
+                    forkL.setEnUs(false);
+                    System.out.printf("Filòsof: fil%s deixa l'esquerra (%d) i espera (dreta ocupada)\n", getName(), forkL.getNumF());
+                    passarGana();
+                }
+            } else {
+                // cas 3: tampoc pot menjar, cap cobert disponible
+                dura(500, 1001);
+            }
+            // si no pot començar a menjar passa a pensar directament
+            pensar();
+        }
+    }
+
+    public Forquilla getForkR() {
+        return forkR;
+    }
+
+    public void setForkR(Forquilla forkR) {
+        this.forkR = forkR;
+    }
+    
+    public Forquilla getForkL() {
+        return forkL;
+    }
+
+    public void setForkL(Forquilla forkL) {
+        this.forkL = forkL;
+    }
 }
--- 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Taula.java
+++ 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Taula.java
@@ -7,17 +7,49 @@ public class Taula {
     public Taula(int numFilosofs, int numForquilles) {
         this.comensals = new Filosof[numFilosofs];
         this.forquilles = new Forquilla[numForquilles];
+        crearForquilles();
         crearFilosofs();
-        crearAssignaForquilles();
+        assignarForquilles();
     }
-    // mostrar tots els filòsofs i les forquilles que pot accedir
-    public void showTaula() {}
-    // inicia l'execució dels fils dels filòsofs existents
-    public void cridarATaula() {}
+    public void crearForquilles() {
+        for (int i = 0; i < forquilles.length; i++) forquilles[i] = new Forquilla(i);
+    }
+    public void crearFilosofs() {
+        for (int i = 0; i < comensals.length; i++) comensals[i] = new Filosof(i);
+    }
+    public void assignarForquilles() {
+        int indexForquilla = 0;
+        // per cada comensal, li assignem 2 forquilles.
+        for (int d = 0; d < comensals.length; d++) {
+            for (int i = indexForquilla; i < forquilles.length; i++) {
+                Forquilla esq = forquilles[i];
+                // ultima forquilla = 0
+                Forquilla dret = forquilles[(i + 1) % forquilles.length];
+                comensals[d].setForkL(esq);
+                comensals[d].setForkR(dret);
+                if (d < comensals.length) {
+                    // actualitzem index forquilla pel pròxim comensal
+                    indexForquilla = i + 1;
+                    break;
+                }
+            }
+        } 
+    }
+    // mostrar tots els filòsofs que ja té les forquilles assignades i que pot accedir a les instàncies
+    public void showTaula() {
+        for (Filosof fil : comensals) {
+            System.out.printf("\nComensal:fil%s esq:%s dret:%s", 
+                                fil.getName(), fil.getForkL().getNumF(), fil.getForkR().getNumF());
+        }
+        System.out.println("\n--------------------------");
+    }
+    public void cridarATaula() {
+        for (Filosof fil : comensals) { fil.start(); }
+    }
 
     public static void main(String[] args) {
-        // completar aquesta part
-        // showTaula();
-        // cridarATaula();
+        Taula taula = new Taula(4, 4);
+        taula.showTaula();
+        taula.cridarATaula();
     }
 }
