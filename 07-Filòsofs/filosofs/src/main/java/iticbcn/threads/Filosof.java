7926f279b112386324447992aa3c133fce9e7bc4
Lishi Ji Liu <liulishij@gmail.com>
2025-02-13 23:55:52 +0100

    07-Filòsofs finalitzat, requilibrat, tots megen.

--- 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Filosof.java
+++ 07-Filòsofs/filosofs/src/main/java/iticbcn/threads/Filosof.java
@@ -14,18 +14,39 @@ public class Filosof extends Thread {
         this.gana = 0;
         this.rnd = new Random();
     }
-
+    
     public void menjar() {
-        System.out.printf("Filòsof: fil%s menja\n", getName());
-        dura(1000, 2001);
-        System.out.printf("Filòsof: fil%s ha acabat de menjar\n", getName());
-        this.gana = 0;
+        if (!forkL.isEnUs()) {
+            forkL.setEnUs(true);
+            System.out.printf("Filòsof: fil%s agafa la forquilla esquerra %d\n", getName(), forkL.getNumF());
+            // cas 1 : pot menjar
+            if (!forkR.isEnUs()) {
+                forkR.setEnUs(true);
+                System.out.printf("Filòsof: fil%s agafa la forquilla dreta %d\n", getName(), forkR.getNumF());
+                System.out.printf("Filòsof: fil%s menja\n", getName());
+                dura(1000, 2001);
+                System.out.printf("Filòsof: fil%s ha acabat de menjar\n", getName());
+                // torna a deixar forquilles
+                forkL.setEnUs(false);
+                forkR.setEnUs(false);
+                this.gana = 0;
+            } else {
+                dura(500, 1001);
+                forkL.setEnUs(false);
+                // cas 2: no pot menjar, no serveix 1 sola forquilla
+                System.out.printf("Filòsof: fil%s deixa l'esquerra (%d) i espera (dreta ocupada)\n", getName(), forkL.getNumF());
+                passarGana();
+            }
+        } else {
+            // cas 3: tampoc pot menjar, cap cobert disponible
+            dura(500, 1001);
+        }
     }
     public void pensar() {
         System.out.printf("Filòsof: fil%s pensant\n", getName());
         dura(1000, 2001);
     }
-
+    
     public void dura(int org, int limit) {
         try {
             sleep(rnd.nextInt(org, limit));
@@ -37,35 +58,13 @@ public class Filosof extends Thread {
     public void passarGana() {
         gana++;
         System.out.printf("Filòsof: fil%s gana=%d\n", getName(), gana);
-        dura(500, 1001);
     }
-
+    
     @Override
     public void run() {
         // menja i pensa alternativament
         while (true) {
-            if (!forkL.isEnUs()) {
-                forkL.setEnUs(true);
-                System.out.printf("Filòsof: fil%s agafa la forquilla esquerra %d\n", getName(), forkL.getNumF());
-                // cas 1 : pot menjar
-                if (!forkR.isEnUs()) {
-                    forkR.setEnUs(true);
-                    System.out.printf("Filòsof: fil%s agafa la forquilla dreta %d\n", getName(), forkR.getNumF());
-                    menjar();
-                    // torna a deixar forquilles
-                    forkL.setEnUs(false);
-                    forkR.setEnUs(false);
-                } else {
-                    // cas 2: no pot menjar, no serveix 1 sola forquilla
-                    forkL.setEnUs(false);
-                    System.out.printf("Filòsof: fil%s deixa l'esquerra (%d) i espera (dreta ocupada)\n", getName(), forkL.getNumF());
-                    passarGana();
-                }
-            } else {
-                // cas 3: tampoc pot menjar, cap cobert disponible
-                dura(500, 1001);
-            }
-            // si no pot començar a menjar passa a pensar directament
+            menjar();
             pensar();
         }
     }
