412d116a0f67f9fb125e2e56282cb35a2376debc
Lishi Ji Liu <liulishij@gmail.com>
2025-02-27 18:55:37 +0100

    10-Fumador fet a classe

---
+++ 10-Fumador/wait_notifyall/src/main/java/iticbcn/threads/Estanc.java
@@ -0,0 +1,16 @@
+package iticbcn.threads;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estanc {
+    private List<Tabac> tabacs;
+    private List<Llumi> llumins;
+    private List<Paper> papers;
+
+    public Estanc() {
+        this.tabacs = new ArrayList<>();
+        this.llumins = new ArrayList<>();
+        this.papers = new ArrayList<>();
+    }
+}
---
+++ 10-Fumador/wait_notifyall/src/main/java/iticbcn/threads/Paper.java
@@ -0,0 +1,5 @@
+package iticbcn.threads;
+
+public class Paper {
+
+}
